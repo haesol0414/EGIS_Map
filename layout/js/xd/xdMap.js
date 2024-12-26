9ea5f86356999dc8adfc97c6b39a56c3879ff4d2
haesol <hsler404@gmail.com>
2024-12-26 17:57:31 +0900

    feat: xd world 지도 로딩

--- layout/js/xd/xdMap.js
+++ layout/js/xd/xdMap.js
@@ -1,30 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const distanceBtn = document.getElementById("distance-btn");
+
+    distanceBtn.addEventListener("click", function() {
+        if (Module && Module.XDSetMouseState) {
+            Module.XDSetMouseState(Module.MML_ANALYS_DISTANCE_STRAIGHT);
+        } else {
+            console.error("Module is not initialized yet!");
+        }
+    });
+});
+
 var Module = {
-    locateFile : function(s) {
-        return "https://cdn.xdworld.kr/latest/"+ s;
+    locateFile: function(s) {
+        return "https://cdn.xdworld.kr/latest/" + s;
     },
     postRun: function() {
-        // Call engine initialization API(essential)
         Module.initialize({
             container: document.getElementById("xd-map"),
-            terrain : {
-                dem : {
-                    url : "https://xdworld.vworld.kr",
-                    name : "dem",
-                    servername : "XDServer3d",
-                    encoding : true
+            terrain: {
+                dem: {
+                    url: "https://xdworld.vworld.kr",
+                    name: "dem",
+                    servername: "XDServer3d",
+                    encoding: true
                 },
-                image : {
-                    url : "https://xdworld.vworld.kr",
-                    name : "tile",
-                    servername : "XDServer3d"
+                image: {
+                    url: "https://xdworld.vworld.kr",
+                    name: "tile",
+                    servername: "XDServer3d"
                 }
             },
-            worker : {
-                use : true,
-                path : "./worker/XDWorldWorker.js",
-                count : 5
-            },
-            defaultKey : "DFG~EpIREQDmdJe1E9QpdBca#FBSDJFmdzHoe(fB4!e1E(JS1I=="
+            defaultKey: "DFG~EpIREQDmdJe1E9QpdBca#FBSDJFmdzHoe(fB4!e1E(JS1I=="
         });
 
         // Set camera position
